83b331c4a9f8e3b32d017c9bf52ca796ecc4fa03
Francesco Neri <francesconeri95@gmail.com>
2023-09-14 15:19:11 +0200

    added grads plots

--- Grid/src/meridional_process_group.py
+++ Grid/src/meridional_process_group.py
@@ -52,7 +52,32 @@ class MeridionalProcessGroup:
             self.s = np.concatenate((self.s, obj.s), axis=0)
             self.M = np.concatenate((self.M, obj.M), axis=0)
 
-    def contour(self, save_filename=None):
+    def assemble_field_gradients(self):
+        self.drho_dr = self.group[0].drho_dr
+        self.drho_dz = self.group[0].drho_dz
+        self.dur_dr = self.group[0].dur_dr
+        self.dur_dz = self.group[0].dur_dz
+        self.dut_dr = self.group[0].dut_dr
+        self.dut_dz = self.group[0].dut_dz
+        self.duz_dr = self.group[0].duz_dr
+        self.duz_dz = self.group[0].duz_dz
+        self.dp_dr = self.group[0].dp_dr
+        self.dp_dz = self.group[0].dp_dz
+
+        for obj in self.group[1:]:
+            self.drho_dr = np.concatenate((self.drho_dr, obj.drho_dr), axis=0)
+            self.drho_dz = np.concatenate((self.drho_dz, obj.drho_dz), axis=0)
+            self.dur_dr = np.concatenate((self.dur_dr, obj.dur_dr), axis=0)
+            self.dur_dz = np.concatenate((self.dur_dz, obj.dur_dz), axis=0)
+            self.dut_dr = np.concatenate((self.dut_dr, obj.dut_dr), axis=0)
+            self.dut_dz = np.concatenate((self.dut_dz, obj.dut_dz), axis=0)
+            self.duz_dr = np.concatenate((self.duz_dr, obj.duz_dr), axis=0)
+            self.duz_dz = np.concatenate((self.duz_dz, obj.duz_dz), axis=0)
+            self.dp_dr = np.concatenate((self.dp_dr, obj.dp_dr), axis=0)
+            self.dp_dz = np.concatenate((self.dp_dz, obj.dp_dz), axis=0)
+
+
+    def contour_fields(self, save_filename=None):
 
         plt.figure(figsize=fig_size)
         plt.contourf(self.z_grid, self.r_grid, self.rho*self.group[0].data.rho_ref, cmap='jet', levels=N_levels_2)
@@ -125,5 +150,98 @@ class MeridionalProcessGroup:
         plt.title(r'$M \ \mathrm{[-]}$')
         if save_filename is not None:
             plt.savefig(folder_name + save_filename + '_M.pdf', bbox_inches='tight')
+
+
+    def contour_field_gradients(self, save_filename=None):
+
+        plt.figure(figsize=fig_size)
+        plt.contourf(self.z_grid, self.r_grid, self.drho_dr, cmap='jet', levels=N_levels_2)
+        plt.colorbar()
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(r'$\partial \rho / \partial r \ \mathrm{[-]}$')
+        if save_filename is not None:
+            plt.savefig(folder_name + save_filename + '_drho_dr.pdf', bbox_inches='tight')
+
+        plt.figure(figsize=fig_size)
+        plt.contourf(self.z_grid, self.r_grid, self.drho_dz, cmap='jet', levels=N_levels_2)
+        plt.colorbar()
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(r'$\partial \rho / \partial {z} \ \mathrm{[-]}$')
+        if save_filename is not None:
+            plt.savefig(folder_name + save_filename + '_drho_dz.pdf', bbox_inches='tight')
+
+        plt.figure(figsize=fig_size)
+        plt.contourf(self.z_grid, self.r_grid, self.dur_dr, cmap='jet', levels=N_levels_2)
+        plt.colorbar()
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(r'$\partial u_r / \partial r \ \mathrm{[-]}$')
+        if save_filename is not None:
+            plt.savefig(folder_name + save_filename + '_dur_dr.pdf', bbox_inches='tight')
+
+        plt.figure(figsize=fig_size)
+        plt.contourf(self.z_grid, self.r_grid, self.dur_dz, cmap='jet', levels=N_levels_2)
+        plt.colorbar()
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(r'$\partial u_r / \partial {z} \ \mathrm{[-]}$')
+        if save_filename is not None:
+            plt.savefig(folder_name + save_filename + '_dur_dz.pdf', bbox_inches='tight')
+
+        plt.figure(figsize=fig_size)
+        plt.contourf(self.z_grid, self.r_grid, self.dut_dr, cmap='jet', levels=N_levels_2)
+        plt.colorbar()
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(r'$\partial u_{\theta} / \partial r \ \mathrm{[-]}$')
+        if save_filename is not None:
+            plt.savefig(folder_name + save_filename + '_dut_dr.pdf', bbox_inches='tight')
+
+        plt.figure(figsize=fig_size)
+        plt.contourf(self.z_grid, self.r_grid, self.dut_dz, cmap='jet', levels=N_levels_2)
+        plt.colorbar()
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(r'$\partial u_{\theta} / \partial {z} \ \mathrm{[-]}$')
+        if save_filename is not None:
+            plt.savefig(folder_name + save_filename + '_dut_dz.pdf', bbox_inches='tight')
+
+        plt.figure(figsize=fig_size)
+        plt.contourf(self.z_grid, self.r_grid, self.duz_dr, cmap='jet', levels=N_levels_2)
+        plt.colorbar()
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(r'$\partial u_z / \partial r \ \mathrm{[-]}$')
+        if save_filename is not None:
+            plt.savefig(folder_name + save_filename + '_duz_dr.pdf', bbox_inches='tight')
+
+        plt.figure(figsize=fig_size)
+        plt.contourf(self.z_grid, self.r_grid, self.duz_dz, cmap='jet', levels=N_levels_2)
+        plt.colorbar()
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(r'$\partial u_z / \partial {z} \ \mathrm{[-]}$')
+        if save_filename is not None:
+            plt.savefig(folder_name + save_filename + '_duz_dz.pdf', bbox_inches='tight')
+
+        plt.figure(figsize=fig_size)
+        plt.contourf(self.z_grid, self.r_grid, self.dp_dr, cmap='jet', levels=N_levels_2)
+        plt.colorbar()
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(r'$\partial p / \partial r \ \mathrm{[-]}$')
+        if save_filename is not None:
+            plt.savefig(folder_name + save_filename + '_dp_dr.pdf', bbox_inches='tight')
+
+        plt.figure(figsize=fig_size)
+        plt.contourf(self.z_grid, self.r_grid, self.dp_dz, cmap='jet', levels=N_levels_2)
+        plt.colorbar()
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(r'$\partial p / \partial {z} \ \mathrm{[-]}$')
+        if save_filename is not None:
+            plt.savefig(folder_name + save_filename + '_dp_dz.pdf', bbox_inches='tight')
 
 
