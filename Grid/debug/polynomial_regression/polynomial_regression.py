fixed averaged flux plots x axis

Grid/debug/polynomial_regression/polynomial_regression.py
@@ -6,7 +6,7 @@ import numpy as np
 import pickle
 
 from Grid.src.polynomial_ls_regression import *
-
+from Grid.src.styles import color_map
 
 # FUNCTION TO REGRESS AND TEST. 4th ORDER SHOULD BE CAPTURED PERFECTLY
 
@@ -50,46 +50,53 @@ Znew_dx = regression_evaluation(Wdx, coeff_vector, nx, ny)
 Znew_dy = regression_evaluation(Wdy, coeff_vector, nx, ny)
 
 fig, ax = plt.subplots(1, 2, figsize=(12, 5))
-contour1 = ax[0].contourf(X, Y, Z, levels=50)
+contour1 = ax[0].contourf(X, Y, Z, levels=50, cmap=color_map)
 fig.colorbar(contour1, ax=ax[0])
 ax[0].set_title(r'$z$')
-contour2 = ax[1].contourf(X, Y, Znew, levels=50)
+contour2 = ax[1].contourf(X, Y, Znew, levels=50, cmap=color_map)
 fig.colorbar(contour2, ax=ax[1])
 ax[1].set_title(r'$z_R$')
-# plt.show()
+fig.savefig('pictures/function_and_regression.pdf', bbox_inches='tight')
+
 
 fig, ax = plt.subplots(2, 2, figsize=(12, 8))
-contour1 = ax[0, 0].contourf(X, Y, Zdx, levels=50)
+contour1 = ax[0, 0].contourf(X, Y, Zdx, levels=50, cmap=color_map)
 fig.colorbar(contour1, ax=ax[0, 0])
 ax[0, 0].set_title(r'$\partial z / \partial x$')
-contour2 = ax[0, 1].contourf(X, Y, Zdy, levels=50)
+contour2 = ax[0, 1].contourf(X, Y, Zdy, levels=50, cmap=color_map)
 fig.colorbar(contour2, ax=ax[0, 1])
 ax[0, 1].set_title(r'$\partial z / \partial y$')
-contour3 = ax[1, 0].contourf(X, Y, Znew_dx, levels=50)
+contour3 = ax[1, 0].contourf(X, Y, Znew_dx, levels=50, cmap=color_map)
 fig.colorbar(contour3, ax=ax[1, 0])
 ax[1, 0].set_title(r'$\partial z_R / \partial x$')
-contour4 = ax[1, 1].contourf(X, Y, Znew_dy, levels=50)
+contour4 = ax[1, 1].contourf(X, Y, Znew_dy, levels=50, cmap=color_map)
 fig.colorbar(contour4, ax=ax[1, 1])
 ax[1, 1].set_title(r'$\partial z_R / \partial y$')
-# plt.show()
+fig.savefig('pictures/derivatives_and_regression.pdf', bbox_inches='tight')
 
 
 # PLOT THE ERRORS
 err = np.abs(Z - Znew)
 plt.figure(figsize=(8, 5))
-plt.contourf(X, Y, err, levels=50)
+plt.contourf(X, Y, err, levels=50, cmap=color_map)
 plt.colorbar()
 plt.title(r'$z(x,y)$ error')
+plt.savefig('pictures/function_error.pdf', bbox_inches='tight')
+
 
 err_x = np.abs(Zdx - Znew_dx)
 plt.figure(figsize=(8, 5))
-plt.contourf(X, Y, err_x, levels=50)
+plt.contourf(X, Y, err_x, levels=50, cmap=color_map)
 plt.colorbar()
 plt.title(r'$\partial z / \partial x$ error')
+plt.savefig('pictures/dfdx_error.pdf', bbox_inches='tight')
+
 
 err_y = np.abs(Zdy - Znew_dy)
 plt.figure(figsize=(8, 5))
-plt.contourf(X, Y, err_y, levels=50)
+plt.contourf(X, Y, err_y, levels=50, cmap=color_map)
 plt.colorbar()
 plt.title(r'$\partial z / \partial y$ error')
+plt.savefig('pictures/dfdy_error.pdf', bbox_inches='tight')
+
 plt.show()

Grid/src/meridional_process.py
@@ -66,7 +66,7 @@ class MeridionalProcess:
         self.camber_normal_check = np.sqrt(self.camber_normal_r ** 2 + self.camber_normal_theta ** 2 +
                                            self.camber_normal_z ** 2)
 
-    def circumferential_average(self, mode='cell centered', fix_borders=False, bfm=None, gauss_filter=False, threshold=50):
+    def circumferential_average(self, mode='cell centered', fix_borders=False, bfm=None, gauss_filter=False, threshold=100):
         """
         perform circumferential averages
         Args:
@@ -1359,10 +1359,12 @@ class MeridionalProcess:
         if save_fig:
             plt.savefig('pictures/u_meridional_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
 
-        # plt.figure(figsize=self.picture_size)
-        # plt.contourf(self.z_cg, self.r_cg, self.ds_dl, cmap=color_map, levels=N_levels)
-        # plt.colorbar()
-        # plt.title(r'$\frac{\partial s}{\partial m}$')
+        plt.figure(figsize=self.picture_size)
+        plt.contourf(self.z_cg, self.r_cg, self.ds_dl, cmap=color_map, levels=N_levels)
+        plt.colorbar()
+        plt.title(r'$\frac{\partial s}{\partial m}$')
+        if save_fig:
+            plt.savefig('pictures/ds_dl_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
 
         plt.figure(figsize=self.picture_size)
         plt.contourf(self.z_cg, self.r_cg, self.Floss, cmap=color_map, levels=N_levels)
@@ -1371,52 +1373,66 @@ class MeridionalProcess:
         if save_fig:
             plt.savefig('pictures/F_loss_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
 
-        # plt.figure(figsize=self.picture_size)
-        # plt.contourf(self.z_cg, self.r_cg, self.Floss_r, cmap=color_map, levels=N_levels)
-        # plt.colorbar()
-        # plt.title(r'$F_{l,r}$')
-        #
-        # plt.figure(figsize=self.picture_size)
-        # plt.contourf(self.z_cg, self.r_cg, self.Floss_t, cmap=color_map, levels=N_levels)
-        # plt.colorbar()
-        # plt.title(r'$F_{l,\theta}$')
-        #
-        # plt.figure(figsize=self.picture_size)
-        # plt.contourf(self.z_cg, self.r_cg, self.Floss_z, cmap=color_map, levels=N_levels)
-        # plt.colorbar()
-        # plt.title(r'$F_{l,z}$')
+        plt.figure(figsize=self.picture_size)
+        plt.contourf(self.z_cg, self.r_cg, self.Floss_r, cmap=color_map, levels=N_levels)
+        plt.colorbar()
+        plt.title(r'$F_{l,r}$')
+        if save_fig:
+            plt.savefig('pictures/Fl_r_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
+
+        plt.figure(figsize=self.picture_size)
+        plt.contourf(self.z_cg, self.r_cg, self.Floss_t, cmap=color_map, levels=N_levels)
+        plt.colorbar()
+        plt.title(r'$F_{l,\theta}$')
+        if save_fig:
+            plt.savefig('pictures/Fl_t_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
+
+        plt.figure(figsize=self.picture_size)
+        plt.contourf(self.z_cg, self.r_cg, self.Floss_z, cmap=color_map, levels=N_levels)
+        plt.colorbar()
+        plt.title(r'$F_{l,z}$')
+        if save_fig:
+            plt.savefig('pictures/Fl_z_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
 
         self.compute_Ftheta()
 
-        # plt.figure(figsize=self.picture_size)
-        # plt.contourf(self.z_cg, self.r_cg, self.drut_dl, cmap=color_map, levels=N_levels)
-        # plt.colorbar()
-        # plt.title(r'$\frac{\partial (r u_{\theta})}{\partial m}$')
-        #
-        # plt.figure(figsize=self.picture_size)
-        # plt.contourf(self.z_cg, self.r_cg, self.Ftheta, cmap=color_map, levels=N_levels)
-        # plt.colorbar()
-        # plt.title(r'$F_{\theta}$')
-        # if save_fig:
-        #     plt.savefig('pictures/F_theta_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
+        plt.figure(figsize=self.picture_size)
+        plt.contourf(self.z_cg, self.r_cg, self.drut_dl, cmap=color_map, levels=N_levels)
+        plt.colorbar()
+        plt.title(r'$\frac{\partial (r u_{\theta})}{\partial m}$')
+        if save_fig:
+            plt.savefig('pictures/drut_dl_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
+
+        plt.figure(figsize=self.picture_size)
+        plt.contourf(self.z_cg, self.r_cg, self.Ftheta, cmap=color_map, levels=N_levels)
+        plt.colorbar()
+        plt.title(r'$F_{\theta}$')
+        if save_fig:
+            plt.savefig('pictures/F_theta_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
 
         self.compute_Fturn()
 
-        # plt.figure(figsize=self.picture_size)
-        # plt.contourf(self.z_cg, self.r_cg, self.Fturn_r, cmap=color_map, levels=N_levels)
-        # plt.colorbar()
-        # plt.title(r'$F_{t, r}$')
-        #
-        # plt.figure(figsize=self.picture_size)
-        # plt.contourf(self.z_cg, self.r_cg, self.Fturn_t, cmap=color_map, levels=N_levels)
-        # plt.colorbar()
-        # plt.title(r'$F_{t, \theta}$')
-        #
-        # plt.figure(figsize=self.picture_size)
-        # plt.contourf(self.z_cg, self.r_cg, self.Fturn_z, cmap=color_map, levels=N_levels)
-        # plt.colorbar()
-        # plt.title(r'$F_{t, z}$')
-        #
+        plt.figure(figsize=self.picture_size)
+        plt.contourf(self.z_cg, self.r_cg, self.Fturn_r, cmap=color_map, levels=N_levels)
+        plt.colorbar()
+        plt.title(r'$F_{t, r}$')
+        if save_fig:
+            plt.savefig('pictures/Fturn_r_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
+
+        plt.figure(figsize=self.picture_size)
+        plt.contourf(self.z_cg, self.r_cg, self.Fturn_t, cmap=color_map, levels=N_levels)
+        plt.colorbar()
+        plt.title(r'$F_{t, \theta}$')
+        if save_fig:
+            plt.savefig('pictures/Fturn_t_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
+
+        plt.figure(figsize=self.picture_size)
+        plt.contourf(self.z_cg, self.r_cg, self.Fturn_z, cmap=color_map, levels=N_levels)
+        plt.colorbar()
+        plt.title(r'$F_{t, z}$')
+        if save_fig:
+            plt.savefig('pictures/Fturn_z_%d_%d.pdf' % (self.nstream, self.nspan), bbox_inches='tight')
+
         plt.figure(figsize=self.picture_size)
         plt.contourf(self.z_cg, self.r_cg, self.Fturn, cmap=color_map, levels=N_levels)
         plt.colorbar()

Grid/src/meridional_process_group.py
@@ -419,6 +419,10 @@ class MeridionalProcessGroup:
                 tmp_len += np.sqrt((z[istream] - z[istream - 1]) ** 2 + (r[istream] - r[istream - 1]) ** 2)
                 self.stream_line_length[istream, ispan] = tmp_len
 
+        #reports to zero the first length of the bladed zone
+        self.middle_line_length = self.stream_line_length[:, self.nspan//2]
+        self.middle_line_length /= self.group[1].stream_line_length[-1, self.group[1].nspan//2]
+
 
     def plot_stream_line(self, field, n, save_filename=None):
         """
@@ -458,12 +462,13 @@ class MeridionalProcessGroup:
 
     def plot_averaged_fluxes(self, field, save_filename=None):
         old_value = 0
+        reference_point = self.middle_line_length[self.group[0].nstream]  # initial reference is the leading edge of the blade
         fig, ax = plt.subplots(figsize=fig_size)
         for obj in self.group:
-
-            # calcualte the streamline length (x arrays) of that subcomponent
-            x = old_value + obj.stream_line_length[:, obj.nspan//2] + obj.stream_line_length[1, obj.nspan//2]
-            old_value = x[-1]
+            begin = old_value  # begin index
+            end = begin + obj.nstream  # end index
+            x = self.middle_line_length[begin:end] - reference_point  # 0 is at leading edge of blade
+            old_value = end
 
             # plots
             if field == 'rho':

Sun/test/iris/main_compressor.py
@@ -8,10 +8,10 @@ from scipy.sparse.linalg import eigs
 
 
 # import the data from the pickle meridional object
-filename = '../../../Grid/testcases/iris/data/meta/inlet_10_15_blade_30_15_outlet_10_15.pickle'
+filename = '../../../Grid/testcases/iris/data/meta/inlet_5_blade_20_outlet_10_nspan_10.pickle'
 with open(filename, "rb") as file:
     meridional_obj = pickle.load(file)
-
+m = 1 # circumferential order
 
 
 #%%sun model
@@ -39,7 +39,7 @@ sun_obj.ComputeJacobianPhysical(routine=gradient_routine, order=gradient_order)
 sun_obj.ContourTransformation(save_filename='iris_blade_jacobians_grid_35_15')
 sun_obj.AddAMatrixToNodesFrancesco2()
 sun_obj.AddBMatrixToNodesFrancesco2()
-sun_obj.AddCMatrixToNodesFrancesco2(m=1)
+sun_obj.AddCMatrixToNodesFrancesco2(m=m)
 sun_obj.AddEMatrixToNodesFrancesco2()
 sun_obj.AddRMatrixToNodesFrancesco2()
 sun_obj.AddSMatrixToNodes()
@@ -79,12 +79,10 @@ eigenvalues *= omega_ref
 marker_size = 20
 fig, ax = plt.subplots(figsize=(7, 5))
 ax.scatter(eigenvalues.real/omega_ref, eigenvalues.imag/omega_ref, marker='o', facecolors='red', edgecolors='red',
-           s=marker_size, label=r'numerical')
+           s=marker_size, label='m: %i' %(m))
 ax.set_xlabel(r'RS [-]')
 ax.set_ylabel(r'DF [-]')
 ax.legend()
-# ax.set_xlim([7500, 35000])
-# ax.set_ylim([-8000, 8000])
 ax.grid(alpha=0.3)
 # fig.savefig('pictures/%i/chi_map_arnoldi_%i_%i_%i.pdf' % (eigenvalues[0].real, Nz, Nr, eigenvalues[0].real), bbox_inches='tight')
 