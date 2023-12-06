9b8c783fc60ac50aae17449c572ff3906c8bc36c
Francesco Neri <francesconeri95@gmail.com>
2023-12-06 13:34:55 +0100

    update

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-from distutils.core import setup
-from distutils.extension import Extension
-import Spakozvsky
-import Grid
-import Sun
-import os
-import platform
+from setuptools import setup, find_packages
 
-
-setup(name='unsflow',
-      version='1.0.0',
-      license='MIT',
-      description='Unsflow is a software for compressor instabilities analysis and prediction',
-      author='Francesco Neri',
-      packages=['unsflow'])
+setup(
+    name='unsflow',
+    version='1.0.0',
+    license='MIT',
+    description='Unsflow is a software for compressor instabilities analysis and prediction',
+    author='Francesco Neri',
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'matplotlib',
+        'pandas',
+        'sympy'
+        # other dependencies...
+    ],
+    packages=find_packages(),
+)
