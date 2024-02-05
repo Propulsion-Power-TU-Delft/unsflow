c35f2a64cff9b6f1df2c7104fb2905556da02076
Francesco Neri <francesconeri95@gmail.com>
2024-02-05 14:43:37 +0100

    update before duct multiblock

--- Sun/test/annular_duct/multiblock/main.py
+++ Sun/test/annular_duct/multiblock/main.py
@@ -61,7 +61,7 @@ for ii in range(0, Nz):
 
 config = Config('duct.ini')
 duct_Obj1 = Sun.src.AnnulusMeridional(0, L, r1, r2, Nz, Nr,
-                                     density.copy(), radialVel.copy(), tangentialVel.copy(), axialVel.copy(), pressure.copy(), config)
+                                     density, radialVel, tangentialVel, axialVel, pressure, config)
 duct_Obj2 = Sun.src.AnnulusMeridional(L/2, L, r1, r2, Nz, Nr,
                                      density, radialVel, tangentialVel, axialVel, pressure, config)
 duct_Obj1.normalize_data()
