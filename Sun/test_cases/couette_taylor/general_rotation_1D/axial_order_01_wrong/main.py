e8eb2e4b1167c65cedb1083a0e73738f6a32d045
Francesco Neri <francesconeri95@gmail.com>
2024-03-15 13:39:44 +0100

    corrected couette sun model 1D

--- Sun/test_cases/couette_taylor/general_rotation_1D/axial_order_01_wrong/main.py
+++ Sun/test_cases/couette_taylor/general_rotation_1D/axial_order_01_wrong/main.py
@@ -13,10 +13,11 @@ from numpy import pi, sin, cos
 import os
 from Grid.src.config import Config
 from Utils.styles import *
+from Sun.src.general_functions import GaussLobattoPoints, ChebyshevDerivativeMatrixBayliss
 
 # input data of the problem (SI units)
 R1 = 1  # inner radius [m]
-D = 0.0005  # radial gap [m]
+D = 0.001  # radial gap [m]
 R2 = R1 + D  # outer radius [m]
 P1 = 100e3  # pressure at hub [Pa]
 T = 288  # temperature everywhere [K]
@@ -24,7 +25,7 @@ L = 3 * D  # length [m]
 GMMA = 1.4  # cp/cv ratio of air
 R = 287.05  # air gas constant [kj/kgK]
 RHO1 = P1/R/T  # density everywhere [kg/m3]
-MU = 1  # omega ratio
+MU = -1  # omega ratio = omega2/omega1
 OMEGA2 = 10  # omega outer cylinder [rad/s]
 OMEGA1 = OMEGA2 / MU  # omega inner cylinder [rad/s]
 HARMONIC_ORDER = 0  # axisymmetric perturbations
@@ -32,7 +33,7 @@ plots = False
 
 # non-dimensionalization terms:
 x_ref = R1
-omega_ref = (OMEGA1 + OMEGA2) / 2
+omega_ref = OMEGA2
 u_ref = omega_ref * x_ref
 rho_ref = RHO1
 t_ref = x_ref / u_ref
@@ -48,35 +49,35 @@ p_ref = rho_ref * u_ref ** 2
 
 
 # %%%%%%%%%%%%%%%%%%%%%%% ANALYTICAL PART %%%%%%%%%%%%%%%%%%%%%%%
-LIMIT = 10
-radial_orders = np.linspace(1, LIMIT, LIMIT)
-axial_orders = np.linspace(2, 2, 1)
-
-
-class EigenFrequency:
-    """class who stores the information of a mode. For now consider only the positive frequencies"""
-
-    def __init__(self, radial_order, axial_order, eigenfrequency_pos, eigenfrequency_neg):
-        self.r_ord = radial_order
-        self.z_ord = axial_order
-        self.omega_pos = eigenfrequency_pos
-        self.omega_neg = eigenfrequency_neg
-
-
-eigs_an_list = []
-for m in radial_orders:
-    for alpha in axial_orders:
-        omega_plus = (2 * OMEGA1 * (pi * alpha * D / L) ** 2) / ((pi * alpha * D / L) ** 2 + m ** 2 * pi ** 2)
-        omega_minus = -(2 * OMEGA1 * (pi * alpha * D / L) ** 2) / ((pi * alpha * D / L) ** 2 + m ** 2 * pi ** 2)
-        eigs_an_list.append(EigenFrequency(m, alpha, omega_plus, omega_minus))
-
-plt.figure()
-for eig in eigs_an_list:
-    plt.scatter(eig.omega_pos.real, eig.omega_pos.imag, c='r', s=50)
-    plt.scatter(eig.omega_neg.real, eig.omega_neg.imag, c='b', s=50)
-plt.xlabel(r'$\omega_R \ \rm{[rad/s]}$')
-plt.ylabel(r'$\omega_I \ \rm{[rad/s]}$')
-plt.grid(alpha=grid_opacity)
+LIMIT = 50
+# radial_orders = np.linspace(1, LIMIT, LIMIT)
+# axial_orders = np.linspace(1, 1, 1)
+#
+#
+# class EigenFrequency:
+#     """class who stores the information of a mode. For now consider only the positive frequencies"""
+#
+#     def __init__(self, radial_order, axial_order, eigenfrequency_pos, eigenfrequency_neg):
+#         self.r_ord = radial_order
+#         self.z_ord = axial_order
+#         self.omega_pos = eigenfrequency_pos
+#         self.omega_neg = eigenfrequency_neg
+#
+#
+# eigs_an_list = []
+# for m in radial_orders:
+#     for alpha in axial_orders:
+#         omega_plus = (2 * OMEGA1 * (pi * alpha * D / L) ** 2) / ((pi * alpha * D / L) ** 2 + m ** 2 * pi ** 2)
+#         omega_minus = -(2 * OMEGA1 * (pi * alpha * D / L) ** 2) / ((pi * alpha * D / L) ** 2 + m ** 2 * pi ** 2)
+#         eigs_an_list.append(EigenFrequency(m, alpha, omega_plus, omega_minus))
+#
+# plt.figure()
+# for eig in eigs_an_list:
+#     plt.scatter(eig.omega_pos.real, eig.omega_pos.imag, c='r', s=50)
+#     plt.scatter(eig.omega_neg.real, eig.omega_neg.imag, c='b', s=50)
+# plt.xlabel(r'$\omega_R \ \rm{[rad/s]}$')
+# plt.ylabel(r'$\omega_I \ \rm{[rad/s]}$')
+# plt.grid(alpha=grid_opacity)
 
 
 
@@ -141,13 +142,20 @@ if plots:
     plt.savefig('pictures/%02i/dp_dr.pdf' % N, bbox_inches='tight')
 
     # plt.show()
-k_axial = axial_orders[0]*pi/(L)
+
 duct_obj.normalize_data()
-alpha_axial = 2
+alpha_axial = 10
+k_axial = pi*alpha_axial/(L/x_ref)
+a_axial = k_axial*D/x_ref
 sun_obj = Sun.src.SunModel1D(duct_obj, config=config)
-sun_obj.compute_L0(k_axial)
-sun_obj.compute_L1()
-sun_obj.compute_L2(k_axial)
+sun_obj.L0 = np.zeros((N, N))
+for i in range(N):
+    sun_obj.L0[i] = a_axial**2 * (1-(1-MU)*duct_obj.zeta[i]/x_ref)
+sun_obj.L1 = np.zeros_like(sun_obj.L0)
+xi = GaussLobattoPoints(N)
+D = ChebyshevDerivativeMatrixBayliss(xi)
+D2 = D@D
+sun_obj.L2 = -2*D2-a_axial**2
 sun_obj.set_boundary_conditions()
 sun_obj.apply_boundary_conditions_generalized()
 
@@ -167,9 +175,9 @@ marker_size = 125
 fig, ax = plt.subplots()
 ax.scatter(eigenvalues.real, eigenvalues.imag, marker='o', facecolors='none', edgecolors='red', s=marker_size,
            label=r'numerical')
-for eig in eigs_an_list:
-    plt.scatter(eig.omega_pos.real, eig.omega_pos.imag, marker='x', c='blue', s=marker_size)
-    plt.scatter(eig.omega_neg.real, eig.omega_neg.imag, marker='x', c='blue', s=marker_size)
+# for eig in eigs_an_list:
+#     plt.scatter(eig.omega_pos.real, eig.omega_pos.imag, marker='x', c='blue', s=marker_size)
+#     plt.scatter(eig.omega_neg.real, eig.omega_neg.imag, marker='x', c='blue', s=marker_size)
 ax.set_xlabel(r'$\omega_{R}$ [rad/s]', fontsize=font_labels)
 ax.set_ylabel(r'$\omega_{I}$ [rad/s]', fontsize=font_labels)
 ax.set_xlim([-5, 5])
@@ -179,7 +187,7 @@ plt.yticks(fontsize=font_axes)
 ax.legend(fontsize=font_legend)
 ax.grid(alpha=grid_opacity)
 fig.savefig('pictures/%02i/chi_map_arnoldi.pdf' % (N), bbox_inches='tight')
-plt.show()
+# plt.show()
 
 # EIGENFUNCTIONS
 r = sun_obj.data.r
--- Sun/test_cases/couette_taylor/solid_body_rotation_1D/axial_order_05/main.py
+++ Sun/test_cases/couette_taylor/solid_body_rotation_1D/axial_order_05/main.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import Sun
 import scipy
-from numpy import pi, sin, cos
+from numpy import pi, sin, cos, sqrt
 import os
 from Grid.src.config import Config
 from Utils.styles import *
@@ -25,7 +25,7 @@ L = 3 * D  # length [m]
 GMMA = 1.4  # cp/cv ratio of air
 R = 287.05  # air gas constant [kj/kgK]
 RHO1 = P1/R/T  # density everywhere [kg/m3]
-MU = -1  # omega ratio = omega2/omega1
+MU = 1  # omega ratio
 OMEGA2 = 10  # omega outer cylinder [rad/s]
 OMEGA1 = OMEGA2 / MU  # omega inner cylinder [rad/s]
 HARMONIC_ORDER = 0  # axisymmetric perturbations
@@ -33,7 +33,7 @@ plots = False
 
 # non-dimensionalization terms:
 x_ref = R1
-omega_ref = OMEGA2
+omega_ref = (OMEGA1 + OMEGA2) / 2
 u_ref = omega_ref * x_ref
 rho_ref = RHO1
 t_ref = x_ref / u_ref
@@ -49,35 +49,35 @@ p_ref = rho_ref * u_ref ** 2
 
 
 # %%%%%%%%%%%%%%%%%%%%%%% ANALYTICAL PART %%%%%%%%%%%%%%%%%%%%%%%
-LIMIT = 50
-# radial_orders = np.linspace(1, LIMIT, LIMIT)
-# axial_orders = np.linspace(1, 1, 1)
-#
-#
-# class EigenFrequency:
-#     """class who stores the information of a mode. For now consider only the positive frequencies"""
-#
-#     def __init__(self, radial_order, axial_order, eigenfrequency_pos, eigenfrequency_neg):
-#         self.r_ord = radial_order
-#         self.z_ord = axial_order
-#         self.omega_pos = eigenfrequency_pos
-#         self.omega_neg = eigenfrequency_neg
-#
-#
-# eigs_an_list = []
-# for m in radial_orders:
-#     for alpha in axial_orders:
-#         omega_plus = (2 * OMEGA1 * (pi * alpha * D / L) ** 2) / ((pi * alpha * D / L) ** 2 + m ** 2 * pi ** 2)
-#         omega_minus = -(2 * OMEGA1 * (pi * alpha * D / L) ** 2) / ((pi * alpha * D / L) ** 2 + m ** 2 * pi ** 2)
-#         eigs_an_list.append(EigenFrequency(m, alpha, omega_plus, omega_minus))
-#
-# plt.figure()
-# for eig in eigs_an_list:
-#     plt.scatter(eig.omega_pos.real, eig.omega_pos.imag, c='r', s=50)
-#     plt.scatter(eig.omega_neg.real, eig.omega_neg.imag, c='b', s=50)
-# plt.xlabel(r'$\omega_R \ \rm{[rad/s]}$')
-# plt.ylabel(r'$\omega_I \ \rm{[rad/s]}$')
-# plt.grid(alpha=grid_opacity)
+LIMIT = 20
+radial_orders = np.linspace(1, LIMIT, LIMIT)
+axial_orders = np.linspace(5, 5, 1)
+
+
+class EigenFrequency:
+    """class who stores the information of a mode. For now consider only the positive frequencies"""
+
+    def __init__(self, radial_order, axial_order, eigenfrequency_pos, eigenfrequency_neg):
+        self.r_ord = radial_order
+        self.z_ord = axial_order
+        self.omega_pos = eigenfrequency_pos
+        self.omega_neg = eigenfrequency_neg
+
+
+eigs_an_list = []
+for m in radial_orders:
+    for alpha in axial_orders:
+        omega_plus = (2 * OMEGA1 * (pi * alpha * D / L)) / sqrt(((pi * alpha * D / L) ** 2 + m ** 2 * pi ** 2))
+        omega_minus = -(2 * OMEGA1 * (pi * alpha * D / L)) / sqrt(((pi * alpha * D / L) ** 2 + m ** 2 * pi ** 2))
+        eigs_an_list.append(EigenFrequency(m, alpha, omega_plus, omega_minus))
+
+plt.figure()
+for eig in eigs_an_list:
+    plt.scatter(eig.omega_pos.real, eig.omega_pos.imag, c='r', s=50)
+    plt.scatter(eig.omega_neg.real, eig.omega_neg.imag, c='b', s=50)
+plt.xlabel(r'$\omega_R \ \rm{[rad/s]}$')
+plt.ylabel(r'$\omega_I \ \rm{[rad/s]}$')
+plt.grid(alpha=grid_opacity)
 
 
 
@@ -144,18 +144,18 @@ if plots:
     # plt.show()
 
 duct_obj.normalize_data()
-alpha_axial = 10
-k_axial = pi*alpha_axial/(L/x_ref)
-a_axial = k_axial*D/x_ref
+a_axial = axial_orders[0]*pi*D/L
 sun_obj = Sun.src.SunModel1D(duct_obj, config=config)
-sun_obj.L0 = np.zeros((N, N))
-for i in range(N):
-    sun_obj.L0[i] = a_axial**2 * (1-(1-MU)*duct_obj.zeta[i]/x_ref)
+
+# governing equations in L0.L1,L2 form
+sun_obj.L0 = np.eye(sun_obj.nPoints)*4*(OMEGA1/omega_ref)**2 * a_axial**2
 sun_obj.L1 = np.zeros_like(sun_obj.L0)
-xi = GaussLobattoPoints(N)
+
+xi = GaussLobattoPoints(sun_obj.data.nPoints)
 D = ChebyshevDerivativeMatrixBayliss(xi)
 D2 = D@D
-sun_obj.L2 = -2*D2-a_axial**2
+sun_obj.L2 = 4 * D2 -np.eye(N)*a_axial**2
+
 sun_obj.set_boundary_conditions()
 sun_obj.apply_boundary_conditions_generalized()
 
@@ -175,19 +175,53 @@ marker_size = 125
 fig, ax = plt.subplots()
 ax.scatter(eigenvalues.real, eigenvalues.imag, marker='o', facecolors='none', edgecolors='red', s=marker_size,
            label=r'numerical')
-# for eig in eigs_an_list:
-#     plt.scatter(eig.omega_pos.real, eig.omega_pos.imag, marker='x', c='blue', s=marker_size)
-#     plt.scatter(eig.omega_neg.real, eig.omega_neg.imag, marker='x', c='blue', s=marker_size)
+kk = 0
+for eig in eigs_an_list:
+    if kk == 0:
+        plt.scatter(eig.omega_pos.real, eig.omega_pos.imag, marker='x', c='blue', s=marker_size, label='analytical')
+    else:
+        plt.scatter(eig.omega_pos.real, eig.omega_pos.imag, marker='x', c='blue', s=marker_size)
+    plt.scatter(eig.omega_neg.real, eig.omega_neg.imag, marker='x', c='blue', s=marker_size)
+    kk += 1
 ax.set_xlabel(r'$\omega_{R}$ [rad/s]', fontsize=font_labels)
 ax.set_ylabel(r'$\omega_{I}$ [rad/s]', fontsize=font_labels)
-ax.set_xlim([-5, 5])
+# ax.set_xlim([-5, 5])
 ax.set_ylim([-1, 1])
 plt.xticks(fontsize=font_axes)
 plt.yticks(fontsize=font_axes)
 ax.legend(fontsize=font_legend)
 ax.grid(alpha=grid_opacity)
-fig.savefig('pictures/%02i/chi_map_arnoldi.pdf' % (N), bbox_inches='tight')
-# plt.show()
+fig.savefig('pictures/%02i/eigenvalue_map.pdf' % (N), bbox_inches='tight')
+
+
+fig, ax = plt.subplots()
+ax.scatter(eigenvalues.real, eigenvalues.imag, marker='o', facecolors='none', edgecolors='red', s=marker_size,
+           label=r'numerical')
+kk = 0
+for eig in eigs_an_list:
+    if kk == 0:
+        plt.scatter(eig.omega_pos.real, eig.omega_pos.imag, marker='x', c='blue', s=marker_size, label='analytical')
+    else:
+        plt.scatter(eig.omega_pos.real, eig.omega_pos.imag, marker='x', c='blue', s=marker_size)
+    plt.scatter(eig.omega_neg.real, eig.omega_neg.imag, marker='x', c='blue', s=marker_size)
+    kk += 1
+ax.set_xlabel(r'$\omega_{R}$ [rad/s]', fontsize=font_labels)
+ax.set_ylabel(r'$\omega_{I}$ [rad/s]', fontsize=font_labels)
+ax.set_xlim([1.6, 2])
+ax.set_ylim([-1, 1])
+plt.xticks(fontsize=font_axes)
+plt.yticks(fontsize=font_axes)
+ax.legend(fontsize=font_legend)
+ax.grid(alpha=grid_opacity)
+fig.savefig('pictures/%02i/eigenvalue_map_zoom.pdf' % (N), bbox_inches='tight')
+
+plt.show()
+
+
+
+
+
+
 
 # EIGENFUNCTIONS
 r = sun_obj.data.r
