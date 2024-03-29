7d6fd2c817b4b6f2f6f89a652976070aca9070e6
Francesco Neri <francesconeri95@gmail.com>
2024-03-29 11:10:19 +0100

    added script for grid pdf generation

---
+++ Grid/debug/gauss_lobatto_grid/main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Sun.src.general_functions import GaussLobattoPoints
+
+Nz = 25
+Nr = 15
+
+xi = GaussLobattoPoints(Nz)
+eta = GaussLobattoPoints(Nr)
+
+XI, ETA = np.meshgrid(xi, eta, indexing='ij')
+
+plt.figure()
+for ii in range(Nz):
+    plt.plot(XI[ii, :], ETA[ii, :], 'k', lw=1)
+for jj in range(Nr):
+    plt.plot(XI[:, jj], ETA[:, jj], 'k', lw=1)
+plt.xticks([])
+plt.yticks([])
+plt.savefig('pictures/gauss_lobatto_grid_%02i_%02i.pdf' %(Nz, Nr), bbox_inches='tight')
+plt.show()
