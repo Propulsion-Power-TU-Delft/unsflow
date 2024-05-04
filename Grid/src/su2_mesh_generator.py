72c555e631ef23f25ff85129c462d535864343b4
Francesco Neri <francesconeri95@gmail.com>
2024-05-04 17:08:35 +0200

    added 3d sector annulus mesh generator

--- Grid/src/su2_mesh_generator.py
+++ Grid/src/su2_mesh_generator.py
@@ -25,12 +25,19 @@ def generate_SU2mesh(*coords, kind_elem, kind_bound, full_annulus=False, filenam
     elif ndim == 3 and kind_elem == 12 and kind_bound == 9:
         if full_annulus:
             generate_3Dmesh_full(X, Y, Z, filename)
-        # else:
-        #     generate_3Dmesh_sector(X, Y, Z, filename)
-
+        else:
+            generate_3Dmesh_sector(X, Y, Z, filename)
+    else:
+        raise ValueError('Mesh type not implemented yet.')
 
 
 def generate_2Dmesh_quads(X, Y, filename):
+    """
+    Generate 2d su2 mesh file for quads elements, with line boundaries.
+    :param X: array of x
+    :param Y: array of y
+    :param filename: name of the mesh.su2 to save
+    """
     nNode = X.shape[0]
     mNode = X.shape[1]
     Mesh_File = open(filename, "w")
@@ -251,3 +258,138 @@ def generate_3Dmesh_full(X, Y, Z, filename):
 
     Mesh_File.close()
 
+
+def generate_3Dmesh_sector(X, Y, Z, filename):
+    """
+    Compute the sector of the annulus mesh, starting from 3D arrays.
+    The first and last k planes are related through periodic boundary conditions.
+    :param X: 3d arrays of x
+    :param Y: 3d arrays of y
+    :param Z: 3d arrays of z
+    :param filename: name of the mesh.su2 to save
+    """
+    KindElem = 12  # Quad
+    KindBound = 9  # Line
+
+    # Store the number of nodes and open the output mesh file
+    nNode = X.shape[0]
+    mNode = X.shape[1]
+    lNode = X.shape[2]
+    Mesh_File = open(filename, "w")
+
+    # Write the dimension of the problem and the number of interior elements
+    Mesh_File.write("%\n")
+    Mesh_File.write("% Problem dimension\n")
+    Mesh_File.write("%\n")
+    Mesh_File.write("NDIME= 3\n")
+    Mesh_File.write("%\n")
+    Mesh_File.write("% Inner element connectivity\n")
+    Mesh_File.write("%\n")
+    Mesh_File.write("NELEM= %s\n" % ((nNode - 1) * (mNode - 1) * (lNode - 1)))
+
+    # Write the element connectivity
+    iElem = 0
+    for iNode in range(nNode - 1):
+        for jNode in range(mNode - 1):
+            for kNode in range(lNode - 1):
+                zero = kNode + jNode * lNode + iNode * lNode * mNode
+                one = kNode + (jNode + 1) * lNode + iNode * lNode * mNode
+                two = kNode + (jNode + 1) * lNode + (iNode + 1) * lNode * mNode
+                three = kNode + jNode * lNode + (iNode + 1) * lNode * mNode
+                four = kNode + 1 + jNode * lNode + iNode * lNode * mNode
+                five = kNode + 1 + (jNode + 1) * lNode + iNode * lNode * mNode
+                six = kNode + 1 + (jNode + 1) * lNode + (iNode + 1) * lNode * mNode
+                seven = kNode + 1 + jNode * lNode + (iNode + 1) * lNode * mNode
+                Mesh_File.write("%s \t %s \t %s \t %s \t %s \t %s \t %s \t %s \t %s\n" % (
+                    KindElem, zero, one, two, three, four, five, six, seven))
+                iElem = iElem + 1
+
+    # Compute the number of nodes and write the node coordinates
+    nPoint = nNode * mNode * lNode
+    Mesh_File.write("%\n")
+    Mesh_File.write("% Node coordinates\n")
+    Mesh_File.write("%\n")
+    Mesh_File.write("NPOIN= %s\n" % (nNode * mNode * lNode))
+    iPoint = 0
+    for iNode in range(nNode):
+        for jNode in range(mNode):
+            for kNode in range(lNode):
+                Mesh_File.write("%15.14f \t %15.14f \t %15.14f \t %s\n" % (
+                    X[iNode, jNode, kNode], Y[iNode, jNode, kNode], Z[iNode, jNode, kNode], iPoint))
+                iPoint = iPoint + 1
+
+    # Write the header information for the boundary markers
+    Mesh_File.write("%\n")
+    Mesh_File.write("% Boundary elements\n")
+    Mesh_File.write("%\n")
+    Mesh_File.write("NMARK= 4\n")
+
+    # Write the boundary information for each marker
+    Mesh_File.write("MARKER_TAG= HUB\n")
+    Mesh_File.write("MARKER_ELEMS= %s\n" % ((nNode - 1) * (lNode - 1)))
+    for iNode in range(nNode - 1):
+        for kNode in range(lNode - 1):
+            jNode = 0
+            zero = kNode + jNode * lNode + iNode * lNode * mNode
+            three = kNode + jNode * lNode + (iNode + 1) * lNode * mNode
+            four = kNode + 1 + jNode * lNode + iNode * lNode * mNode
+            seven = kNode + 1 + jNode * lNode + (iNode + 1) * lNode * mNode
+            Mesh_File.write("%s \t %s \t %s \t %s \t %s\n" % (KindBound, zero, four, seven, three))
+
+    Mesh_File.write("MARKER_TAG= OUTLET\n")
+    Mesh_File.write("MARKER_ELEMS= %s\n" % ((mNode - 1) * (lNode - 1)))
+    for jNode in range(mNode - 1):
+        for kNode in range(lNode - 1):
+            iNode = nNode - 2
+            two = kNode + (jNode + 1) * lNode + (iNode + 1) * lNode * mNode
+            three = kNode + jNode * lNode + (iNode + 1) * lNode * mNode
+            six = kNode + 1 + (jNode + 1) * lNode + (iNode + 1) * lNode * mNode
+            seven = kNode + 1 + jNode * lNode + (iNode + 1) * lNode * mNode
+            Mesh_File.write("%s \t %s \t %s \t %s \t %s\n" % (KindBound, two, six, seven, three))
+
+    Mesh_File.write("MARKER_TAG= SHROUD\n")
+    Mesh_File.write("MARKER_ELEMS= %s\n" % ((nNode - 1) * (lNode - 1)))
+    for iNode in range(nNode - 1):
+        for kNode in range(lNode - 1):
+            jNode = mNode - 2
+            one = kNode + (jNode + 1) * lNode + iNode * lNode * mNode
+            two = kNode + (jNode + 1) * lNode + (iNode + 1) * lNode * mNode
+            five = kNode + 1 + (jNode + 1) * lNode + iNode * lNode * mNode
+            six = kNode + 1 + (jNode + 1) * lNode + (iNode + 1) * lNode * mNode
+            Mesh_File.write("%s \t %s \t %s \t %s \t %s\n" % (KindBound, one, two, six, five))
+
+    Mesh_File.write("MARKER_TAG= INLET\n")
+    Mesh_File.write("MARKER_ELEMS= %s\n" % ((mNode - 1) * (lNode - 1)))
+    for jNode in range(mNode - 1):
+        for kNode in range(lNode - 1):
+            iNode = 0
+            zero = kNode + jNode * lNode + iNode * lNode * mNode
+            one = kNode + (jNode + 1) * lNode + iNode * lNode * mNode
+            four = kNode + 1 + jNode * lNode + iNode * lNode * mNode
+            five = kNode + 1 + (jNode + 1) * lNode + iNode * lNode * mNode
+            Mesh_File.write("%s \t %s \t %s \t %s \t %s\n" % (KindBound, zero, one, five, four))
+
+    Mesh_File.write("MARKER_TAG= PER0\n")
+    Mesh_File.write("MARKER_ELEMS= %s\n" % ((mNode - 1) * (nNode - 1)))
+    for iNode in range(nNode - 1):
+        for jNode in range(mNode - 1):
+            kNode = 0
+            zero = kNode + jNode * lNode + iNode * lNode * mNode
+            one = kNode + (jNode + 1) * lNode + iNode * lNode * mNode
+            two = kNode + (jNode + 1) * lNode + (iNode + 1) * lNode * mNode
+            three = kNode + jNode * lNode + (iNode + 1) * lNode * mNode
+            Mesh_File.write("%s \t %s \t %s \t %s \t %s\n" % (KindBound, zero, three, two, one))
+
+    Mesh_File.write("MARKER_TAG= PER1\n")
+    Mesh_File.write("MARKER_ELEMS= %s\n" % ((mNode - 1) * (nNode - 1)))
+    for iNode in range(nNode - 1):
+        for jNode in range(mNode - 1):
+            kNode = lNode - 2
+            four = kNode + 1 + jNode * lNode + iNode * lNode * mNode
+            five = kNode + 1 + (jNode + 1) * lNode + iNode * lNode * mNode
+            six = kNode + 1 + (jNode + 1) * lNode + (iNode + 1) * lNode * mNode
+            seven = kNode + 1 + jNode * lNode + (iNode + 1) * lNode * mNode
+            Mesh_File.write("%s \t %s \t %s \t %s \t %s\n" % (KindBound, four, seven, six, five))
+
+    # Close the mesh file and exit
+    Mesh_File.close()
