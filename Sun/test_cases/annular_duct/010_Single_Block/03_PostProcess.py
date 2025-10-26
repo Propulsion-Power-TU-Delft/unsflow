40623f56ddec958098622e9023d990f326321775
Francesco Neri <francesconeri95@gmail.com>
2025-10-26 19:17:09 +0100

    just moved bunch of files

---
+++ Sun/test_cases/annular_duct/010_Single_Block/03_PostProcess.py
@@ -0,0 +1,20 @@
+from Sun.src.sun_post_process import PostProcessSun
+import matplotlib.pyplot as plt
+import pickle
+
+
+pp3010 = PostProcessSun('Output/results_30_10.pkl')
+pp3010.extract_eigenfields()
+pp3010.plot_eigenfields()
+
+omegar_an = [13450, 21077, 26721, 31296, 35049]
+omegai_an = [0, 0, 0, 0, 0]
+
+plt.figure()
+plt.plot(omegar_an, omegai_an, 'x', mfc='none', ms=10, label='Analytical')
+plt.plot(pp3010.data['Eigenfrequencies'].real, pp3010.data['Eigenfrequencies'].imag, 'o', mfc='none', ms=10, label=r'$30 \times 10$')
+plt.legend()
+plt.grid(alpha=0.2)
+plt.xlim([12000, 36000])
+plt.ylim([-5000, 5000])
+plt.show()
