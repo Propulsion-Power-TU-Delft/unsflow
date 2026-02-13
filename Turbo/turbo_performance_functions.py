4e490f964abe2ef889a139e79137193273300ad2
Francesco Neri <francesconeri95@gmail.com>
2026-02-13 16:16:22 +0100

    update

--- Turbo/turbo_performance_functions.py
+++ Turbo/turbo_performance_functions.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from CoolProp.CoolProp import PropsSI, PhaseSI
 
-def compute_work_coefficient_temperature_ratio(tratio, omega=None, rpm=None, r_ref=None, cp=1005, Tt1=288.15):
+
+def compute_work_coefficient_temperature_ratio(tratio, r_ref, omega=None, rpm=None, cp=1005, Tt1=288.15):
     if (omega is None and rpm is None):
         raise ValueError("either omega or rpm must be provided")
     elif (rpm is not None):
@@ -45,3 +47,27 @@ def compute_isentropic_efficiency(pratio, tratio, gamma=1.4):
 def compute_temperature_ratio(pratio, efficiency, gamma=1.4):
     tratio = 1 + (pratio**((gamma-1)/gamma) - 1) / efficiency
     return tratio
+
+def compute_work_coefficient_temperature_ratio_real(delta_ht, r_ref, omega=None, rpm=None):
+    if (omega is None and rpm is None):
+        raise ValueError("either omega or rpm must be provided")
+    elif (rpm is not None):
+        omega = 2 * np.pi * rpm / 60
+    else:
+        pass
+    
+    u_ref = omega * r_ref
+    psi = delta_ht / (u_ref**2)
+    return psi
+
+def compute_tip_mach_number_real(r_ref, soundspeed, omega=None, rpm=None):
+    if (omega is None and rpm is None):
+        raise ValueError("either omega or rpm must be provided")
+    elif (rpm is not None):
+        omega = 2 * np.pi * rpm / 60
+    else:
+        pass
+    
+    u_ref = omega * r_ref
+    M_tip = u_ref / soundspeed
+    return M_tip
